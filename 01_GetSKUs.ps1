d86f5756ca9fbf0d731e3ee3511c64808889a562
sccmentor <pwinstanley@gmail.com>
2025-01-04 14:26:56 +0000

    Update 01_GetSKUs.ps1
    Added parameters to Install-Module

--- 01_GetSKUs.ps1
+++ 01_GetSKUs.ps1
@@ -2,7 +2,7 @@
 # Try Get-PSRepository to see all available registered module repositories.'
 # the run the following command Register-PSRepository -Default
 
-Install-Module Microsoft.Graph.Identity.DirectoryManagement
+Install-Module Microsoft.Graph.Identity.DirectoryManagement -Scope CurrentUser -Force -AllowClobber
 Connect-MgGraph -Scopes "Organization.Read.All"
 Get-MgSubscribedSku | Select-Object SkuId, SkuPartNumber
 
