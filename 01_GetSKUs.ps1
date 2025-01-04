f9ecec33236b43c8698774649bafcdbffaaf3953
sccmentor <pwinstanley@gmail.com>
2025-01-04 13:14:09 +0000

    Add files via upload

---
+++ 01_GetSKUs.ps1
@@ -0,0 +1,8 @@
+# Optional - if you receive the error'Install-Package: No match was found for the specified search criteria and module name 'Microsoft.Graph.Identity.DirectoryManagement'. 
+# Try Get-PSRepository to see all available registered module repositories.'
+# the run the following command Register-PSRepository -Default
+
+Install-Module Microsoft.Graph.Identity.DirectoryManagement
+Connect-MgGraph -Scopes "Organization.Read.All"
+Get-MgSubscribedSku | Select-Object SkuId, SkuPartNumber
+
